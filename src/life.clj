8c794942d07a866928b1e3e9d42d3b2c6738d6e4
Jake Shelby <jake.shelby@me.com>
2022-05-08 20:14:01 -0600

    Randomness and Wrapping
    And some memoization for performance

--- src/life.clj
+++ src/life.clj
@@ -1,8 +1,7 @@
 (ns life)
 
-(defn neighbors [{:keys [height width]} [x y]]
-  ;; Currently filters out cells that are out of bounds
-  ;; TODO - wrap around?
+(defn *neighbors-bounded [{:keys [height width]} [x y]]
+  ;; Filters out cells that are out of bounds
   (filter (fn [[x y]]
             (and
               (> x -1)
@@ -18,41 +17,105 @@
            [(inc x) (dec y)]
            [(dec x) (inc y)]]))
 
+(defn *neighbors-wrapped
+  [{:keys [height width]} [x y]]
+  (->> ;; Each of the neighbors
+    [[(dec x) y]
+     [(inc x) y]
+     [x (inc y)]
+     [x (dec y)]
+     [(inc x) (inc y)]
+     [(dec x) (dec y)]
+     [(inc x) (dec y)]
+     [(dec x) (inc y)]]
+    ;; Wrap around to the other side
+    (map (fn [[x y]]
+           (let [x (cond
+                     ;; Wrap to the end
+                     (> 0 x)      (dec width)
+                     ;; Wrap to the begining
+                     (>= x width) 0
+                     ;; In bounds
+                     :else        x)
+                 y (cond
+                     ;; Wrap to the end
+                     (> 0 y)       (dec height)
+                     ;; Wrap to the begining
+                     (>= y height) 0
+                     ;; In bounds
+                     :else         y)]
+             ;; Return new pair
+             [x y])))))
+
+(def neighbors (memoize *neighbors-wrapped))
+
 (defn advance-cell
   [{state :state :as board} coord]
   (let [;; Get Cell state
         cell (get state coord)
         ;; Count Live Neighbors
-        bors (->> (neighbors board coord)
+        bors (->> coord
+                  (neighbors (select-keys board [:height :width]) )
                   (keep state)
                   count)]
     (cond
       ;; Born
       (and (not cell)
-           (= 3 bors))
+           (or
+             ;; (= 2 bors)
+             (= 3 bors)
+             ;; (= 4 bors)
+             )
+           )
       1
+
+
       ;; Live
       (and cell
-           (or (= 2 bors)
-               (= 3 bors)))
+           (or
+             (= 2 bors)
+             (= 3 bors))
+           ;; Random ability to die
+           (< (rand) 0.999)
+           )
       (inc cell)
+
+      ;; Random ability to live (with over-population)
+      ;; (and cell
+      ;;      (= 4 bors)
+      ;;      (= 5 bors)
+      ;;      (> (rand) 0.99)
+      ;;      )
+      ;; (inc cell)
+
+      ;; Random ability to spawn
+      (and (not cell)
+           (> (rand) 0.99999))
+      1
+
+      ;; Random ability to spawn, better when next to someone
+      (and (not cell)
+           (< 0 bors)
+           (> (rand) 0.999))
+      1
+
       ;; Die
-      (and cell
-           (not (or (= 2 bors)
-                    (= 3 bors))))
+      :else
       nil)))
 
-(defn board-coords [{:keys [width height]}]
+
+(defn *board-coords [width height]
   (for [x (range width)
         y (range height)]
     [x y]))
 
+(def board-coords (memoize *board-coords))
+
 ;; Take a board, return next generation
 (defn advance-board
-  [board]
+  [{:keys [width height] :as board}]
   (assoc board :state
-         (->> board
-              board-coords
+         (->> (board-coords width height)
               (map (juxt identity (partial advance-cell board)))
               (filter second)
               (into {}))))
